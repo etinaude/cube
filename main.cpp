4d23597c76507546a05dfc85e78abd85c4e247f3
Etienne Naude <etinaude@gmail.com>
2019-06-24 15:39:18 +1200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <bits/stdc++.h>
+/*  class face {public:
+    private: int center;
+    private: int colour[8];
+    public: int get_colour(int point){
+        return colour[point]; 
+    }
+    public: int set_colour(int new_colour, int point){
+        colour[point] = new_colour;
+        return 0;
+    }
+    public: int display_face(){
+        for(int i = 0; i < 9; i++){
+            printf("%i \n", colour[i]);
+        }
+        return 0;
+    }
+    public: int set_face(int in){
+        center = in;
+         for(int i = 0; i < 9; i++){
+            colour[i] = in;
+        }
+        return 0;
+    }
+};
+
+int create_cube(){
+    face red; red.set_face(2);
+    face orange; orange.set_face(3);
+    face blue; blue.set_face(1);
+    face green; green.set_face(4);
+    face yellow; yellow.set_face(5);
+    face white; white.set_face(0);
+    return 0;
+}
+*/
+
+int main(){
+    printf("TEST");
+   // create_cube();
+  return 0;
+}
